19c7e2eacd889726c3d4ce261f1c4fd5a82083a2
Mehmet Alptekin Isiklar <maisiklar@gmail.com>
2023-08-08 16:46:59 +0300

    still building components. Completed reservation card

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "resizer.otstatic.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+//https://resizer.otstatic.com/v2/photos/xlarge/2/51416250.webp
